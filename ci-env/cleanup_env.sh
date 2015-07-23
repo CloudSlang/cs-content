d068c45ed3f893103d5c40da8714c35227fc6da6
Bonczidai Levente <levente.bonczidai@hp.com>
2015-07-23 11:14:20 +0300

    add cleanup_env script - 2
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hp.com>

--- ci-env/cleanup_env.sh
+++ ci-env/cleanup_env.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # print droplets
-cat < "droplets_${CIRCLE_BUILD_NUM}.txt"
+# cat < "droplets_${CIRCLE_BUILD_NUM}.txt"
 
 DROPLET_ID_ACC=$(cat < "droplets_${CIRCLE_BUILD_NUM}.txt")
-echo "DROPLET_ID_ACC: $DROPLET_ID_ACC"
+# echo "DROPLET_ID_ACC: $DROPLET_ID_ACC"
 
 for DROPLET_ID in $DROPLET_ID_ACC
 do
   CURL_OUTPUT=$(curl -i -s -L -X DELETE -H 'Content-Type: application/json' -H "Authorization: Bearer $DO_API_TOKEN" \
   "https://api.digitalocean.com/v2/droplets/$DROPLET_ID")
-  echo "CURL_OUTPUT: $CURL_OUTPUT"
+  # echo "CURL_OUTPUT: $CURL_OUTPUT"
 
-  STATUS_CODE=$(echo "$CURL_OUTPUT" | grep "status" | awk '{print $2}')
+  STATUS_CODE=$(echo "$CURL_OUTPUT" | grep "Status" | awk '{print $2}')
   echo "STATUS_CODE: $STATUS_CODE"
 
   if [ "$STATUS_CODE" = "204" ]
