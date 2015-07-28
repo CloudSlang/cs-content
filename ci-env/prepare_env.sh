ea9234a57cc82ea0992d3d750907f616758aefa6
Bonczidai Levente <levente.bonczidai@hp.com>
2015-07-28 10:07:24 +0300

    test droplet access 7
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hp.com>

--- ci-env/prepare_env.sh
+++ ci-env/prepare_env.sh
@@ -79,8 +79,7 @@ sed -i "s/<coreos_host>/${DROPLET_IP_ARRAY[0]}/g" test/io/cloudslang/coreos/test
 # create ssh private key
 SSH_KEY_PATH=droplets_rsa
 echo -e "$SSH_PRIVATE_KEY_CI_ENV_TEMP2" > $SSH_KEY_PATH
-ls -l .
-cat droplets_rsa
+# ls -l .
 
 # update inputs files to use actual ssh key
 sed -i "s/<private_key_file>/${SSH_KEY_PATH}/g" test/io/cloudslang/coreos/test_access_coreos_machine.inputs.yaml
