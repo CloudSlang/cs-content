a5ee9e54b12b508cad22c43efdabeff81265526d
Bonczidai Levente <levente.bonczidai@hp.com>
2015-07-30 10:48:51 +0300

    extract create_droplets script
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hp.com>

--- ci-env/prepare_env.sh
+++ ci-env/prepare_env.sh
@@ -6,61 +6,7 @@ inc_and_sleep()
   sleep ${SLEEP_INTERVAL}
 }
 
-# generate discovery URL for the new CoreOS cluster and update the cloud-config file
-DISCOVERY_URL=$(curl -s -X GET "https://discovery.etcd.io/new")
-echo "DISCOVERY_URL: $DISCOVERY_URL"
-DISCOVERY_URL_ESCAPED=$(echo ${DISCOVERY_URL} | sed 's/\//\\\//g')
-sed -i "s/<discovery_url>/${DISCOVERY_URL_ESCAPED}/g" ci-env/cloud-config.yaml
-# cat ci-env/cloud-config.yaml
-
-COREOS_MACHINE_NAMES="\
-ci-${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}-coreos-1 \
-ci-${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}-coreos-2 \
-ci-${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}-coreos-3"
-for COREOS_MACHINE in ${COREOS_MACHINE_NAMES}
-do
-  CURL_OUTPUT=$(curl -i -s -X POST https://api.digitalocean.com/v2/droplets \
-                -H 'Content-Type: application/json' \
-                -H "Authorization: Bearer $DO_API_TOKEN" \
-                -d "{
-                  \"name\":\"${COREOS_MACHINE}\",
-                  \"ssh_keys\":[${DO_DROPLET_SSH_PUBLIC_KEY_ID}],"'
-                  "region":"ams3",
-                  "size":"512mb",
-                  "image":"coreos-stable",
-                  "backups":false,
-                  "ipv6":false,
-                  "private_networking":true,
-                  "user_data": "'"$(cat ci-env/cloud-config.yaml | sed 's/"/\\"/g')"'"
-                }')
-
-  # echo "CURL_OUTPUT: $CURL_OUTPUT"
-
-  STATUS_CODE=$(echo ${CURL_OUTPUT} | awk '{print $2}')
-
-  if [ "$STATUS_CODE" = "202" ]
-  then
-    DROPLET_DETAILS=$(echo "$CURL_OUTPUT" | grep "droplet")
-    #  echo "DROPLET_DETAILS: $DROPLET_DETAILS"
-
-    # split after `:` and `,` characters and extract the droplet ID
-    DROPLET_ID_JUNK_ARRAY=(${DROPLET_DETAILS//:/ })
-    DROPLET_ID_JUNK=${DROPLET_ID_JUNK_ARRAY[2]}
-    DROPLET_ID_ARRAY=(${DROPLET_ID_JUNK//,/ })
-    DROPLET_ID=${DROPLET_ID_ARRAY[0]}
-
-    DROPLET_ID_ACC+="${DROPLET_ID} "
-
-    echo "$COREOS_MACHINE (ID: $DROPLET_ID) droplet creation request accepted"
-  else
-    echo "Problem occurred: $COREOS_MACHINE droplet creation request - status code: $STATUS_CODE"
-  fi
-done
-
-# store droplet IDs in a file to be accessible in cleanup script
-# echo $DROPLET_ID_ACC
-echo ${DROPLET_ID_ACC} > "droplets_${CIRCLE_BUILD_NUM}.txt"
-
+DROPLET_ID_ACC=$(cat "droplets_${CIRCLE_BUILD_NUM}.txt")
 SLEEP_INTERVAL=5 # 5 sec
 TIMEOUT=300 # 5 mins
 
@@ -141,12 +87,7 @@ sed -i "s/<coreos_host>/${DROPLET_IP_ARRAY[0]}/g" test/io/cloudslang/coreos/clus
 # create ssh private key
 SSH_KEY_PATH=droplets_rsa
 echo -e "${DO_DROPLET_SSH_PRIVATE_KEY}" > ${SSH_KEY_PATH}
-cat "${SSH_KEY_PATH}"
-# ls -l .
 
 # update inputs files to use actual ssh key
 sed -i "s/<private_key_file>/${SSH_KEY_PATH}/g" test/io/cloudslang/coreos/test_access_coreos_machine.inputs.yaml
 sed -i "s/<private_key_file>/${SSH_KEY_PATH}/g" test/io/cloudslang/coreos/cluster_docker_images_maintenance.inputs.yaml
-
-cat test/io/cloudslang/coreos/test_access_coreos_machine.inputs.yaml
-cat test/io/cloudslang/coreos/cluster_docker_images_maintenance.inputs.yaml
