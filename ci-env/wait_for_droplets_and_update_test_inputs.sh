9d22b9ebc4e8bca1ddb180df4e80afdcd3a4eeb1
Bonczidai Levente <levente.bonczidai@hp.com>
2015-08-04 16:56:25 +0300

    add restart_container_base_on_usage test case to ci
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hp.com>

--- ci-env/wait_for_droplets_and_update_test_inputs.sh
+++ ci-env/wait_for_droplets_and_update_test_inputs.sh
@@ -82,8 +82,7 @@ done
 # update inputs files to use actual IP addresses
 DROPLET_IP_ARRAY=(${DROPLET_IP_ADDRESS_ACC})
 sed -i "s/<coreos_host>/${DROPLET_IP_ARRAY[0]}/g" test/io/cloudslang/coreos/cluster_docker_images_maintenance.inputs.yaml
-sed -i "s/<cadvisor_host>/${DROPLET_IP_ARRAY[0]}/g" test/io/cloudslang/docker/cadvisor/get_container_metrics.inputs.yaml
-sed -i "s/<cadvisor_host>/${DROPLET_IP_ARRAY[0]}/g" test/io/cloudslang/docker/cadvisor/report_machine_metrics.inputs.yaml
+sed -i "s/<cadvisor_host>/${DROPLET_IP_ARRAY[0]}/g" test/io/cloudslang/docker/cadvisor/*.inputs.yaml
 
 # create ssh private key
 SSH_KEY_PATH=droplets_rsa
@@ -91,5 +90,4 @@ echo -e "${DO_DROPLET_SSH_PRIVATE_KEY}" > ${SSH_KEY_PATH}
 
 # update inputs files to use actual ssh key
 sed -i "s/<private_key_file>/${SSH_KEY_PATH}/g" test/io/cloudslang/coreos/cluster_docker_images_maintenance.inputs.yaml
-sed -i "s/<private_key_file>/${SSH_KEY_PATH}/g" test/io/cloudslang/docker/cadvisor/get_container_metrics.inputs.yaml
-sed -i "s/<private_key_file>/${SSH_KEY_PATH}/g" test/io/cloudslang/docker/cadvisor/report_machine_metrics.inputs.yaml
+sed -i "s/<private_key_file>/${SSH_KEY_PATH}/g" test/io/cloudslang/docker/cadvisor/*.inputs.yaml
