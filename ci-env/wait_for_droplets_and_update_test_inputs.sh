47b7e504daae864c3f586052ffcc0d2c61bf2034
Bonczidai Levente <levente.bonczidai@hp.com>
2015-08-05 17:18:24 +0300

    add quotes to ssh cmd  and newline before echoing SUCCESS
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hp.com>

--- ci-env/wait_for_droplets_and_update_test_inputs.sh
+++ ci-env/wait_for_droplets_and_update_test_inputs.sh
@@ -87,12 +87,11 @@ chmod 0600 ${SSH_KEY_PATH}
 for DROPLET_ID in ${DROPLET_IP_ADDRESS_ACC}
 do
   LAST_LINE=$(ssh -i ${SSH_KEY_PATH} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no core@${DROPLET_ID} \
-  'sudo cat /etc/systemd/system/docker-tcp.socket \
-  && sudo systemctl enable docker-tcp.socket \
+  'sudo systemctl enable docker-tcp.socket \
   && sudo systemctl stop docker \
   && sudo systemctl start docker-tcp.socket \
   && sudo systemctl start docker \
-  && echo 'SUCCESS'')
+  && echo -e "\nSUCCESS"')
 
   echo "SSH output: ${LAST_LINE}"
   if [ "${LAST_LINE}" = "SUCCESS" ]
