eabc5b31ea6a12a0cf8673daf933a42dcac543b2
Bonczidai Levente <levente.bonczidai@hp.com>
2015-08-10 11:27:53 +0300

    init iteration number with 0
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hp.com>

--- ci-env/wait_for_droplets_and_update_test_inputs.sh
+++ ci-env/wait_for_droplets_and_update_test_inputs.sh
@@ -84,12 +84,12 @@ echo -e "${DO_DROPLET_SSH_PRIVATE_KEY}" > ${SSH_KEY_PATH}
 chmod 0600 ${SSH_KEY_PATH}
 
 # enable Docker Remote API on a New Socket - open TCP port
-ITER_NR=-1
+ITER_NR=0
 for DROPLET_IP in ${DROPLET_IP_ADDRESS_ACC}
 do
-  # skip for the first machine because manager needs to use that port
+  # skip the first machine because manager needs to use that port
   ((ITER_NR+=1))
-  if [ "${ITER_NR}" = "0" ]
+  if [ "${ITER_NR}" = "1" ]
   then
     continue
   fi
