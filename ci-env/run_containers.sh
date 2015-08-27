e9221f2dccf1a6478756bb984d59cde7feff2bf0
Bonczidai Levente <levente.bonczidai@hp.com>
2015-08-27 18:08:58 +0300

    update script
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hp.com>

--- ci-env/run_containers.sh
+++ ci-env/run_containers.sh
@@ -2,11 +2,10 @@
 
 echo 'Hello from script!'
 
-# RESULT=$((docker run --privileged -d -p 4444 -p 49153:22 -e PORT=4444 --name docker_host_ssh orius123/dind-ssh && echo -e "\nSUCCESS") | tail -n 1)
+RESULT=$((docker run --privileged -d -p 4444 -p 49153:22 -e PORT=4444 --name docker_host_ssh orius123/dind-ssh && echo -e "\nSUCCESS") | tail -n 1)
 
-#if [ "${RESULT}" = "SUCCESS" ]
-#  then
-#    echo "Droplet(${DROPLET_ID}) deleted successfully"
-#  else
-#    echo "Problem occurred: destroying droplet(${DROPLET_ID}) - status code: ${STATUS_CODE}"
-#  fi
+if [ "${RESULT}" != "SUCCESS" ]
+then
+  echo "Container startup failed.. retrying"
+  docker run --privileged -d -p 4444 -p 49153:22 -e PORT=4444 --name docker_host_ssh orius123/dind-ssh
+fi
