e42c55b5974711bc68c8b0e15f1962db2110f62b
Bonczidai Levente <levente.bonczidai@hp.com>
2015-08-28 09:13:58 +0300

    aux 6
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hp.com>

--- ci-env/run_containers.sh
+++ ci-env/run_containers.sh
@@ -2,7 +2,7 @@
 
 echo 'Hello from script!'
 
-RESULT=$((docker run --privileged -d -p 4444 -p 49153:22 -e PORT=4444 --name docker_host_ssh orius123/dind-ssh && echo -e "\nSUCCESS") | tail -n 1)
+RESULT=$((docker run --privileged -d -p 32124:4444 -p 49153:22 -e PORT=32124 --name docker_host_ssh orius123/dind-ssh && echo -e "\nSUCCESS") | tail -n 1)
 
 if [ "${RESULT}" != "SUCCESS" ]
 then
