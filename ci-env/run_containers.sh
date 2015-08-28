9b5ffe69f3c69c82668e867fe2c6374215f49dd2
Bonczidai Levente <levente.bonczidai@hp.com>
2015-08-28 14:03:54 +0300

    use initial container config
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hp.com>

--- ci-env/run_containers.sh
+++ ci-env/run_containers.sh
@@ -2,17 +2,18 @@
 
 echo 'Hello from script!'
 
-RESULT=$((docker run --privileged -d -p 127.0.0.1:32123:4444 -p 127.0.0.1:32124:22 -e PORT=32123 --name docker_host_ssh orius123/dind-ssh && echo -e "\nSUCCESS") | tail -n 1)
+RESULT=$((docker run --privileged -d -p 4444 -p 49153:22 -e PORT=4444 --name docker_host_ssh orius123/dind-ssh && echo -e "\nSUCCESS") | tail -n 1)
 
 if [ "${RESULT}" != "SUCCESS" ]
 then
   echo "*** Container startup failed.. retrying **"
   docker ps -a
+  echo "*** Removing container **"
   docker stop docker_host_ssh && docker rm docker_host_ssh
   echo "*** Restart Docker ***"
   sudo restart docker
   echo "*** Resetting iptable ***"
   sudo iptables -F
   docker ps -a
-  docker run --privileged -d -p 4444 -p 22 -e PORT=4444 --name docker_host_ssh orius123/dind-ssh
+  docker run --privileged -d -p 4444 -p 49153:22 -e PORT=4444 --name docker_host_ssh orius123/dind-ssh
 fi
