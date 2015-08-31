72fcdc47a5b5b9ccb6602db20a9e77a2096d25e2
Bonczidai Levente <levente.bonczidai@hpe.com>
2015-08-31 09:13:54 +0300

    restart docker service on failure
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hpe.com>

---
+++ ci-env/run_containers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo 'Hello from script!'
+
+RESULT=$((docker run --privileged -d -p 4444 -p 49153:22 -e PORT=4444 --name docker_host_ssh orius123/dind-ssh && echo -e "\nSUCCESS") | tail -n 1)
+
+if [ "${RESULT}" != "SUCCESS" ]
+then
+  echo "Container startup failed.. retrying"
+
+  echo "Removing container:"
+  docker stop docker_host_ssh && docker rm docker_host_ssh
+
+  echo "Restart Docker:"
+  sudo restart docker
+  echo "Resetting iptable:"
+  sudo iptables -F
+
+  echo "Docker status:"
+  sudo service docker status
+
+  echo "Sleeping.."
+  sleep 5
+
+  echo "Docker status:"
+  sudo service docker status
+
+  docker run --privileged -d -p 4444 -p 49153:22 -e PORT=4444 --name docker_host_ssh orius123/dind-ssh
+fi
