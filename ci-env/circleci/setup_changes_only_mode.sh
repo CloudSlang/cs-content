7dd8f0995a459be978c45579b1e11a7194b6a5fb
Bonczidai Levente <levente.bonczidai@hpe.com>
2016-10-13 10:02:06 +0300

    improvements
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hpe.com>

--- ci-env/circleci/setup_changes_only_mode.sh
+++ ci-env/circleci/setup_changes_only_mode.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# parameters to the script:
-#   - COREOS_MACHINE_NAMES - string with machines names separated by space
-#   - DO_API_TOKEN - DigitalOcean personal access token
-#   - DO_DROPLET_SSH_PUBLIC_KEY_ID - ID of the SSH public key in DigitalOcean
-#   - DO_REGION - DigitalOcean region where the droplets should be created
-#   - CLOUD_CONFIG_FILE - path to the cloud-config file
-#   - DROPLETS_FILE - droplets info is stored in this file (e.g. IDs)
-
 git remote -v
 git fetch origin master
 git branch -a
